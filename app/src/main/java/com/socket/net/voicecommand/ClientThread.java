23805aac7ca7df228cf66853194c8bfaacfd6557
bhargav <bhargavbhegde7@gmail.com>
2016-06-20 01:18:42 +0530

    imports added

--- app/src/main/java/com/socket/net/voicecommand/ClientThread.java
+++ app/src/main/java/com/socket/net/voicecommand/ClientThread.java
@@ -1,6 +1,8 @@
 package com.socket.net.voicecommand;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
